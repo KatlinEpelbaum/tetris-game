808323d6a0898b821221ef01b130a430c64c5a8b
KatlinEpelbaum <katlinepelbaum47@gmail.com>
2025-03-07 09:59:37 +0200

    generate new block fix

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 import { GameBoard } from "./src/GameBoard.js";
-import { SBlock } from "./src/SBlock.js"; 
-
+import { SBlock } from "./src/SBlock.js";
 const allBlocks = [SBlock];
 
 const gameBoard = new GameBoard();
-const currentBlock = generateNewBlock();
+let currentBlock = generateNewBlock();
 
 const intervalId = setInterval(run, 700);
 
 function run() {
-
-    if( currentBlock.canMoveDown(gameBoard.height)) {
+    if (currentBlock.canMoveDown(gameBoard.height)) {
         currentBlock.moveDown();
-    }else{
-        currentBlock = generateNewBlock();
+    } else {
+        currentBlock = generateNewBlock();  
     }
     
-    gameBoard.draw(currentBlock)
+    gameBoard.draw(currentBlock);
 }
 
 function generateNewBlock() {
-    
     const i = Math.floor(Math.random() * allBlocks.length);
-
-    return new allBlocks[i];
+    return new allBlocks[i]();
 }
